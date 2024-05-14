2500c25da693bec5d4cde8260d6beaf55f8c5691
AurelienChetot <aurelien.chetot@gmail.com>
2024-05-14 10:04:35 +0200

    step 1 fetch api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,6 +25,8 @@ const router = createBrowserRouter([
       {
         path: "/cupcakes",
         element: <CupcakeList />,
+        // add loader
+        loader: async () => fetch(`http://localhost:3310/api/cupcakes`),
         // Step 1: load data here
       },
     ],
