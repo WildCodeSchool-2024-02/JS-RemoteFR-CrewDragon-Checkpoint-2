48875157779481c2f32389d3f48ec28bf75d8268
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-05-14 10:53:33 +0200

    finish step 2

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -60,7 +60,12 @@ function CupcakeList() {
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
-        {/* Step 2: repeat this block for each cupcake */}
+        {
+          /* Step 2: repeat this block for each cupcake */
+          dataCupcake.map((data) => (
+            <Cupcake key={data.id} data={data} />
+          ))
+        }
         {/* Step 5: filter cupcakes before repeating */}
         <li className="cupcake-item">
           <Cupcake />
