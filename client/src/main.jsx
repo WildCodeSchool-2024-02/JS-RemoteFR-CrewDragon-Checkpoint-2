2996c42785b6a1984e1b00f2af6c60b68fa03142
Franck HERPOUX <franckherpoux@hotmail.fr>
2024-05-14 09:42:19 +0200

    Step 1 done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,7 +25,8 @@ const router = createBrowserRouter([
       {
         path: "/cupcakes",
         element: <CupcakeList />,
-        // Step 1: load data here
+        loader: async () =>
+          fetch("http://localhost:3310/api/cupcakes").then((res) => res.json()),
       },
     ],
   },
